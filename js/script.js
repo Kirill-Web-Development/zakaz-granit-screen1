2b7810c7032e69eb8ccb02c24883580d6cff36ec
Kirill-Web-Development <87230741+Kirill-Web-Development@users.noreply.github.com>
2021-07-22 00:39:39 +0300

    page-1

---
+++ js/script.js
@@ -0,0 +1,75 @@
+$(function() {
+	$('.slider').slick({
+		infinite: true,
+		arrows: false,
+		dots: true
+
+	})
+});
+
+$(function() {
+	$('.slider2').slick({
+		infinite: true,
+		arrows: true,
+		dots: true
+
+	
+
+	})
+});
+
+$(function () {
+	$(window).scroll(function() {
+	    $('.section2__title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.section2-hr').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.section3__title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.section3-hr').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.card__title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+})
+
+
+
